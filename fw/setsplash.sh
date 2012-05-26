8cd19968269b4d16cc8b6210eece20adadc3a0f3
superelchi <superelchi@53b2a5c4-b94e-4333-a725-a9619b392caf>
2012-05-26 12:08:48 +0000

    setsplash.sh fix for older ffmpeg versions
    git-svn-id: svn://svn.code.sf.net/p/dpf-ax/code/trunk@20 53b2a5c4-b94e-4333-a725-a9619b392caf

--- fw/setsplash.sh
+++ fw/setsplash.sh
@@ -4,6 +4,8 @@
 #
 # Simple image to rgb565 conversion & flashing
 #
+# 2012/05/25 superelchi
+#   Added ffmpeg version detection
 # 2012/04/25 superelchi
 #   First version   
 #
@@ -18,28 +20,38 @@ fi
 
 name=${1%.*}
 
+ffmpeg -pix_fmt list 2>/dev/null | grep -q rgb565be
+ret=$?
+if [ $ret -eq 0 ] ; then
+#
+# ffmpeg supports rgb565be
+#
+echo "Using ffmpeg pix_fmt rgb565be"
 ffmpeg -y -f image2 -i "$1" -vcodec rawvideo -f rawvideo -pix_fmt rgb565be -s $2x$3 "${name}.$2x$3.rgb" > ffmpeg.log 2>&1
 ret=$?
 if [ $ret -ne 0 ] ; then
     echo "ffmpeg error: $ret (see file ffmpeg.log)."
     exit 1
 fi
+else
+#
+# ffmpeg does not support rgb565be
+#
+echo "Using ffmpeg pix_fmt rgb565"
+ffmpeg -y -f image2 -i "$1" -vcodec rawvideo -f rawvideo -pix_fmt rgb565 -s $2x$3 "${name}.tmp" > ffmpeg.log 2>&1
+ret=$?
+if [ $ret -ne 0 ] ; then
+    echo "ffmpeg error: $ret (see file ffmpeg.log)."
+    exit 1
+fi
+# for "le" systems:
+dd if="${name}.tmp" of="${name}.$2x$3.rgb" conv=swab >/dev/null 2>&1 
+#
+#for "be" systems:
+#mv "${name}.tmp" "${name}.$2x$3.rgb"
+rm -f "${name}.tmp"
+fi
 rm -f ffmpeg.log
-#
-# If your version of ffmpeg does not support rgb565be, use following commands:
-#
-#ffmpeg -y -f image2 -i ${name}.png -vcodec rawvideo -f rawvideo -pix_fmt rgb565 -s $2x$3 "${name}.tmp"
-#dd if="${name}.tmp" of="${name}.$2x$3.rgb" conv=swab
-#rm -f "${name}.tmp"
-
-#
-# Add width/height at start of .rgb (not used anymore)
-#
-#mv "${name}.$2x$3.rgb" "${name}.tmp"
-#printf "0: %.4x" $2 | sed -E 's/0: (..)(..)/0: \2\1/' | xxd -r -g0 >"${name}.rgb"
-#printf "0: %.4x" $3 | sed -E 's/0: (..)(..)/0: \2\1/' | xxd -r -g0 >>"${name}.rgb"
-#dd bs=1 seek=4 if="${name}.tmp" of="${name}.$2x$3.rgb"
-#rm -f "${name}.tmp"
 
 echo "Splashfile saved to ${name}.$2x$3.rgb"
 if [ $# -ge 4 ] ; then
