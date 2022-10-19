775d2eef42360473ce8df2ad714fc40996d2e2fa
James Buren <braewoods+mgh@braewoods.net>
2022-10-18 22:04:18 -0500

    Fix ffmpeg in setsplash.sh

--- tools/setsplash.sh
+++ tools/setsplash.sh
@@ -20,41 +20,23 @@ fi
 
 name=${1%.*}
 
-ffmpeg -pix_fmt list 2>/dev/null | grep -q rgb565be
+ffmpeg -pix_fmts 2>/dev/null | grep -q rgb565be
 ret=$?
-if [ $ret -eq 0 ] ; then
-#
-# ffmpeg supports rgb565be
-#
-echo "Using ffmpeg pix_fmt rgb565be"
+if [ $ret -ne 0 ]; then
+    echo "ffmpeg does not support rgb565be pix_fmt"
+    exit 1
+fi
+
 ffmpeg -y -f image2 -i "$1" -vcodec rawvideo -f rawvideo -pix_fmt rgb565be -s $2x$3 "${name}.$2x$3.rgb" > ffmpeg.log 2>&1
 ret=$?
 if [ $ret -ne 0 ] ; then
     echo "ffmpeg error: $ret (see file ffmpeg.log)."
     exit 1
 fi
-else
-#
-# ffmpeg does not support rgb565be
-#
-echo "Using ffmpeg pix_fmt rgb565"
-ffmpeg -y -f image2 -i "$1" -vcodec rawvideo -f rawvideo -pix_fmt rgb565 -s $2x$3 "${name}.tmp" > ffmpeg.log 2>&1
-ret=$?
-if [ $ret -ne 0 ] ; then
-    echo "ffmpeg error: $ret (see file ffmpeg.log)."
-    exit 1
-fi
-# for "le" systems:
-dd if="${name}.tmp" of="${name}.$2x$3.rgb" conv=swab >/dev/null 2>&1 
-#
-#for "be" systems:
-#mv "${name}.tmp" "${name}.$2x$3.rgb"
-rm -f "${name}.tmp"
-fi
+
 rm -f ffmpeg.log
 
 echo "Splashfile saved to ${name}.$2x$3.rgb"
 if [ $# -ge 4 ] ; then
     python splash2flash.py "${name}.$2x$3.rgb" $2 $3 "usb0"
 fi
-
